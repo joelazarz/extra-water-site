fbad563e739cbb250b1beb5f790c72ba266eb3d7
joe lazarz <jlazarz87@gmail.com>
2020-02-05 22:09:37 -0500

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,70 @@
+<style>
+
+	h1 {
+		margin: 0 auto;
+		text-align: center;
+		font-size: 2em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	.container {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.column {
+		width: 50%;
+	}
+
+	.space {
+		display: flex;
+		flex-direction: column;
+		flex-basis: 100%;
+		flex: 1;
+		background-color: lightgreen;
+		margin: 2%;
+		}
+
+	/* img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	} */
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 3em;
+		}
+	}
+
+</style>
+
+<svelte:head>
+	<title>Extra Water</title>
+</svelte:head>
+
+<h1>Extra Water</h1>
+
+<div class="container">
+
+	<div class="column">
+		<div class="space">test</div>
+		<div class="space">test</div>
+		<div class="space">test</div>
+	</div>
+
+
+	<div class="column">
+		<div class="space">test</div>
+		<div class="space">test</div>
+		<div class="space">test</div>
+	</div>
+
+</div>
+
+
+
+
+
---
+++ src/routes/voicemail.svelte
@@ -0,0 +1,23 @@
+<style>
+
+	.container {
+		text-align: center;
+	}
+
+	h1 {
+		font-size: 2em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+</style>
+
+<svelte:head>
+	<title>Voicemail</title>
+</svelte:head>
+
+<div class="container">
+	<h1>Voicemail</h1>
+</div>
+
